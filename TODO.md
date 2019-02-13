d57252bfa3beae88747b5724d41e4ce2e703f5db
Devdevdavid <david.devant@live.fr>
2019-02-13 19:47:50 +0100

    Nouvelle structure de dossier des sources

---
+++ TODO.md
@@ -0,0 +1,23 @@
+# TODO
+
+## Objectifs 
+
+Ce fichier à pour but de résumer l'état actuel du projet, les points incompris, les points d'amélioration et les questions éventuelles.
+
+## Questions
+
+- Peut-on supprimer cette ligne suite à la migration à Vivado 2018 ? (cpu_UAL.vhd:7)
+```vhdl
+use work.fixed_generic_pkg_mod.all; -- Fix for Vivado
+```
+
+## Liste des améliorations
+
+### CPU
+
+### VGA
+
+### Toolchain
+
+## Liste des tâches en cours
+
---
+++ ghdlToolchain.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+cc=ghdl
+topLayerName=tb_flipflop
+simuDurationNs=1500
+opt=--ieee=synopsys
+
+src_array=(
+    ./src/register_n_bits.vhd 
+    ./src/mux_2_in.vhd 
+    ./src/flipflop.vhd 
+    ./src/counter_n_bits.vhd 
+    ./src/addi_1_bit.vhd 
+    ./src/addi_n_bits.vhd
+    ./src/ual.vhd
+    ./src/ut.vhd
+    ./src/uc_fsm.vhd
+    ./src/uc.vhd
+    ./src/sync_ram.vhd
+    ./src/cpu.vhd
+
+    ./src/tb/tb_addi_n_bits.vhd
+    ./src/tb/tb_mux_2_in.vhd
+    ./src/tb/tb_ual.vhd
+    ./src/tb/tb_flipflop.vhd
+    ./src/tb/tb_ut.vhd
+    ./src/tb/tb_uc_fsm.vhd
+    ./src/tb/tb_uc.vhd
+    ./src/tb/tb_cpu.vhd
+)
+
+# Clean
+${cc} --clean
+
+# Analyse all files 
+for src in "${src_array[@]}"
+    do ${cc} -a ${opt} ${src}
+done
+
+# Display nice message
+if [ $? -eq 0 ]; then
+    echo "(II) COMPILATION OK"
+else
+    echo "(EE) COMPILATION FAIL"
+    exit
+fi
+
+# Build executable
+${cc} -e ${opt} ${topLayerName}
+
+# Execute
+${cc} -r ${opt} ${topLayerName} --vcd=./vcd/${topLayerName}.vcd --stop-time=${simuDurationNs}ns
+
+# Nice message
+if [ $? -eq 0 ]; then
+    echo "(II) EXECUTION OK"
+else
+    echo "(EE) EXECUTION FAIL"
+    exit
+fi
